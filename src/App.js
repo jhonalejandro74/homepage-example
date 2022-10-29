9d947b5738b4a3dbc3e91503bd5525e3bca47776
jhonalejo <jhonalejo@192.168.0.102>
2022-10-29 13:54:02 -0500

    homepage first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className='AppContainer'>
+      <h1>probando heading styles</h1>
+      <p>probando estilos</p>
+    </div>
+  )
+}
+
+export default App
